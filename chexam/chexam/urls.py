7819c7bb1707287df01259d6d971fa6770be96d0
abdessalam-dai <daihalo70@gmail.com>
2022-05-26 23:51:07 +0100

    Created templates and static folders

--- chexam/chexam/urls.py
+++ chexam/chexam/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('exam/', include('exams.urls')),
+    path('exams/', include('exams.urls')),
 ]

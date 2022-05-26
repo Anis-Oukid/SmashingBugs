ba1213cbc5f3696eb9b32dcf2d5a4eccc0b2f4ec
ObamaOnline <anisoukid0@gmail.com>
2022-05-26 23:31:53 +0100

    account types

--- chexam/chexam/settings.py
+++ chexam/chexam/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'student',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.User'

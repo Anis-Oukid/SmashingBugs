ec3e3de64584d72fd7d23bfc71a8ef9e6555fed9
abdessalam-dai <daihalo70@gmail.com>
2022-05-27 01:51:35 +0100

    added view for results

--- chexam/accounts/models.py
+++ chexam/accounts/models.py
@@ -7,15 +7,24 @@ class Classroom(models.Model):
     year = models.CharField(blank=False, max_length=3)
     number = models.IntegerField(blank=False)
 
+    def __str__(self):
+        return f'Year-{self.year} _ G-{self.number}'
+
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_constraint=False)
     classroom = models.ForeignKey(Classroom, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_constraint=False)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Administrator(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, db_constraint=False)
--- chexam/exams/views.py
+++ chexam/exams/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Result
 
-# Create your views here.
+
+def results_page(request):
+    student = request.user.student
+    results = student.result_set.all()
+
+    context = {
+        'results': results
+    }
+    return render(request, "exams/results_page/index.html", context=context)
